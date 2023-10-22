85299447d6ce1891bd186693d5b8782213a24179
Ben Hsieh <bqh5026@gmail.com>
2023-10-21 22:37:20 -0500

    Initial project setup

---
+++ src/test/java/com/storedprocedures/calls/CallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storedprocedures.calls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
