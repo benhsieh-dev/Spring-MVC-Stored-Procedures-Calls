b244e391c0fe5a82154945a5223bd27583ec1d4b
Ben Hsieh <bqh5026@gmail.com>
2023-10-24 22:28:38 -0500

    Update video entity

--- src/main/java/com/storedprocedures/calls/model/Video.java
+++ src/main/java/com/storedprocedures/calls/model/Video.java
@@ -1,9 +1,17 @@
 package com.storedprocedures.calls.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table
+@Getter
+@Setter
 public class Video {
+	@Id
 	private int id;
 	private String title;
 	private String url;
