7f20eb3f3ff6cb3a85003b9d981e395447d78c07
Ben Hsieh <bqh5026@gmail.com>
2023-10-30 22:56:41 -0500

    Update video dao

--- src/main/java/com/storedprocedures/calls/dao/VideoDao.java
+++ src/main/java/com/storedprocedures/calls/dao/VideoDao.java
@@ -1,5 +1,14 @@
 package com.storedprocedures.calls.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
 public class VideoDao {
+	
+	@Autowired
+	private EntityManager em; 
 
 }
