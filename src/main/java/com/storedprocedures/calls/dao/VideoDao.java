28b60dc3174f3969881ff14758753ffc645a84d8
Ben Hsieh <bqh5026@gmail.com>
2023-10-31 21:42:14 -0500

    Add getVideoInfo method

--- src/main/java/com/storedprocedures/calls/dao/VideoDao.java
+++ src/main/java/com/storedprocedures/calls/dao/VideoDao.java
@@ -1,7 +1,11 @@
 package com.storedprocedures.calls.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.storedprocedures.calls.model.Video;
 
 import jakarta.persistence.EntityManager;
 
@@ -10,5 +14,9 @@ public class VideoDao {
 	
 	@Autowired
 	private EntityManager em; 
+	
+	public List<Video> getVideoInfo() {
+		return em.createStoredProcedureQuery("firstProcedure").getResultList();
+	}
 
 }
