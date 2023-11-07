9c31d4a60b0e300ac06f159cba4d1ae87abbe716
Ben Hsieh <bqh5026@gmail.com>
2023-11-06 19:51:39 -0600

    Update video controller

--- src/main/java/com/storedprocedures/calls/controller/VideoController.java
+++ src/main/java/com/storedprocedures/calls/controller/VideoController.java
@@ -1,14 +1,21 @@
 package com.storedprocedures.calls.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.storedprocedures.calls.dao.VideoDao;
+import com.storedprocedures.calls.model.Video;
 
 @RestController
 public class VideoController {
 	
 	@Autowired
 	private VideoDao dao; 
+	
+	public List<Video> findVideos() {
+		return dao.getVideoInfo(); 
+	}
 
 }
